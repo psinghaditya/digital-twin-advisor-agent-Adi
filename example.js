caeca60c7fb515081f346c1b9ea48d4e558f0608
Aditya Pratap Singh <adityaprataps373@gmail.com>
2026-04-17 01:15:24 +0530

    Improve CLI interaction for agent example

--- example.js
+++ example.js
@@ -1,12 +1,21 @@
 const { runAgent } = require("./agent");
 const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// If question passed as command argument
+const argQuestion = process.argv.slice(2).join(" ");
 
-rl.question("Ask a Digital Twin Question: ", (question) => {
-  runAgent(question);
-  rl.close();
-});
+if (argQuestion) {
+  console.log("Running Digital Twin Advisor Agent...\n");
+  runAgent(argQuestion);
+} else {
+  // Interactive mode
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question("Ask a Digital Twin Question: ", (question) => {
+    runAgent(question);
+    rl.close();
+  });
+}

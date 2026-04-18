2cd0f4aab061e22d7f95d7dd93ea703dd70e80a3
psinghaditya <adityaprataps373@gmail.com>
2026-04-18 15:29:39 +0530

    Update example.js

--- example.js
+++ example.js
@@ -4,18 +4,33 @@ const readline = require("readline");
 // If question passed as command argument
 const argQuestion = process.argv.slice(2).join(" ");
 
+async function startAgent(question) {
+  try {
+    await runAgent(question);
+  } catch (error) {
+    console.error("Agent Error:", error);
+  }
+}
+
 if (argQuestion) {
+
   console.log("Running Digital Twin Advisor Agent...\n");
-  runAgent(argQuestion);
+
+  startAgent(argQuestion);
+
 } else {
-  // Interactive mode
+
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-  rl.question("Ask a Digital Twin Question: ", (question) => {
-    runAgent(question);
+  rl.question("Ask a Digital Twin Question: ", async (question) => {
+
+    await startAgent(question);
+
     rl.close();
+
   });
+
 }
